6fbced8694d507ab43586449bb76dbe01e3cbc39
kleeCollage <klee.collage@gmail.com>
2026-03-15 12:52:43 -0600

    feat: Implement layouts and lazy load on searchPage

---
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -0,0 +1,31 @@
+import { Link, Outlet } from "react-router"
+
+export const HeroesLayout = () => {
+  return (
+    <div className="bg-red-100">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/heroes/1">Hero</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Link to="/admin">Admin</Link>
+        </li>
+      </ul>
+      <section className="mt-10">
+        <Outlet />
+      </section>
+    </div>
+  )
+}
